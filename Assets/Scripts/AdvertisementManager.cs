758c44857b7a2f9502488a0bfcb0a704bd01abe7
Akamis <mrtetex@gmail.com>
2020-03-04 13:52:55 +0100

    Complete Main Menu and add Note Taker

---
+++ Assets/Scripts/AdvertisementManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdvertisementManager : MonoBehaviour
+{
+    
+    public void OpenWebsite(string website_url){
+        Application.OpenURL(website_url);
+    }
+}
