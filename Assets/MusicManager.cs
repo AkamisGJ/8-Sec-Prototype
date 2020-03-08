744195a5e0c3a81c3e0397f7bf1bde0fc9a272d3
Akamis <mrtetex@gmail.com>
2020-03-09 00:36:31 +0100

    Add music

---
+++ Assets/MusicManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Engine.UI;
+
+public class MusicManager : MonoBehaviour
+{
+    UIToggle toogle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        toogle = GetComponent<UIToggle>();
+        int state = PlayerPrefs.GetInt("Mute", 0);
+        if(state == 1)
+        {
+            toogle.IsOn = true;
+        }
+        else
+        {
+            toogle.IsOn = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (toogle.IsOn)
+        {
+            GameMusicPlayer.Instance.GetComponent<AudioSource>().mute = true;
+            PlayerPrefs.SetInt("Mute", 1);
+        }
+        else
+        {
+            GameMusicPlayer.Instance.GetComponent<AudioSource>().mute = false;
+            PlayerPrefs.SetInt("Mute", 0);
+        }
+    }
+
+}
---
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : Singleton<DontDestroyOnLoad>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<AudioSource>())
+        {
+            AudioSource audiosource = GetComponent<AudioSource>();
+            if (audiosource.isPlaying)
+            {
+                return;
+            }
+            else
+            {
+                audiosource.Play();
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+    }
+}
